4ec08ee575ab0d863b70061d9ecd687f7219b6be
Máximo <max.slanza@gmail.com>
2020-09-30 11:37:25 -0300

    Styles

--- src/app/shared/services/menu.service.ts
+++ src/app/shared/services/menu.service.ts
@@ -71,36 +71,37 @@ getTodayPrincipalSweetOption(): OpcionSemana{
   getMenuPrincipal(): Opcion[] {
 
     return [
-      { dia: 1, descripcion: 'Tarta de jamón, puerros y tomates'},
-    { dia: 2, descripcion: 'Carré de cerdo relleno salado con puré de papas'},
-    { dia: 3, descripcion: 'Ravioles de muzarella, lomito y parmesano con salsa capresse'},
-    { dia: 4, descripcion: 'Milanesa de pollo con puré de papas y perejil'},
-    { dia: 5, descripcion: 'Pescado al horno con papas noisette'},
-    { dia: 6, descripcion: 'Lasagna mixta de carne, jamón y muzarella'},
-    { dia: 7, descripcion: 'Muslo de pollo al horno con ensalada rusa'},
-    { dia: 8, descripcion: 'Tarta de la huerta de estación: zuchinis, queso parmesano, cebolla de verdeo, puerros, zanahorias y calabaza'},
-    { dia: 9, descripcion: 'Pastel de carne y papa sobre masa de hojaldre'},
-    { dia: 10, descripcion: 'Ravioles de pollo con salsa de puerros'},
-    { dia: 11, descripcion: 'Suprema de pollo rellena de jamón y queso acompañada de salsa de hongos y ensalada alemana de papas'},
-    { dia: 12, descripcion: 'Croquetas de arroz acompañadas de ensalada de lechugas y tomates'},
-    { dia: 13, descripcion: 'Capeletis con salsa carusso'},
-    { dia: 14, descripcion: 'Estofado de pollo acompañado de arroz'},
-    { dia: 15, descripcion: 'Torta capresse con ensalada de zanahorias ralladas'},
-    { dia: 16, descripcion: 'Bifes a la portuguesa con papas'},
-    { dia: 17, descripcion: 'Ñoquis con tuco clásico'},
-    { dia: 18, descripcion: 'Milanesa de carne acompañada de arroz pilaf'},
-    { dia: 19, descripcion: 'Torta de pollo acompañada de tomates y cebolla colorada'},
-    { dia: 20, descripcion: 'Spaguettis con tuco de pollo'},
-    { dia: 21, descripcion: 'Cazuela de lentejas'},
-    { dia: 22, descripcion: 'Tarta de puerros, cebolla, panceta y queso'},
-    { dia: 23, descripcion: 'Bifes de bondiola en salsa barbacoa con puré de papas'},
-    { dia: 24, descripcion: 'Ravioles de jamón con salsa bolognesa'},
-    { dia: 25, descripcion: 'Brochete de pollo con ensalada de arroz primavera'},
-    { dia: 26, descripcion: 'Milanesa de pescado con ensalada mixta'},
-    { dia: 27, descripcion: 'Tarta de jamón y choclo'},
-    { dia: 28, descripcion: 'Suprema de pollo, acompañada de salsa de puerros y arroz con azafrán'},
-    { dia: 29, descripcion: 'Tarta de jamón, puerros y tomates'},
-    { dia: 30, descripcion: 'Carré de cerdo relleno salado con puré de papas'}
+      { dia: 1, descripcion: 'Ravioles de muzarella, lomito y parmesano con salsa de tomates y albahaca'},
+    { dia: 2, descripcion: 'Milanesa de pollo con puré de papas y perejil'},
+    { dia: 3, descripcion: 'Pescado al horno con papas noisette'},
+    { dia: 4, descripcion: 'Lasagna mixta de carne, jamón y muzarella'},
+    { dia: 5, descripcion: 'Muslo de pollo al horno con ensalada rusa'},
+    { dia: 6, descripcion: 'Tarta de la huerta de estación: zuchinis, queso parmesano, cebolla de verdeo, puerros, zanahorias y calabaza'},
+    { dia: 7, descripcion: 'Pastel de carne y papa sobre masa de hojaldre'},
+    { dia: 8, descripcion: 'Ravioles de pollo con salsa de puerros'},
+    { dia: 9, descripcion: 'Suprema de pollo rellena de jamón y queso acompañada de salsa de hongos y ensalada de papas y perejil'},
+    { dia: 10, descripcion: 'Pan de carne'},
+    { dia: 11, descripcion: 'Capeletis con salsa carusso'},
+    { dia: 12, descripcion: 'Estofado de pollo acompañado de arroz'},
+    { dia: 13, descripcion: 'Torta capresse con ensalada de zanahorias ralladas'},
+    { dia: 14, descripcion: 'Polenta rellena de jamón y muzarella con topping bolognesa'},
+    { dia: 15, descripcion: 'Ravioles de muzarella, lomito y parmesano con salsa de tomates y albahaca '},
+    { dia: 16, descripcion: 'Milanesa de carne acompañada de puré'},
+    { dia: 17, descripcion: 'Croquetas de arroz acompañadas de ensalada de lechugas y tomates'},
+    { dia: 18, descripcion: 'Spaguettis con tuco de pollo'},
+    { dia: 19, descripcion: 'Cazuela de lentejas'},
+    { dia: 20, descripcion: 'Tarta de puerros, cebolla, panceta y queso'},
+    { dia: 21, descripcion: 'Bifes de bondiola en salsa barbacoa con puré de papas'},
+    { dia: 22, descripcion: 'Ravioles de verduras con salsa bolognesa'},
+    { dia: 23, descripcion: 'Brochete de pollo con ensalada de arroz primavera'},
+    { dia: 24, descripcion: 'Milanesa de pescado con ensalada mixta'},
+    { dia: 25, descripcion: 'Tarta de jamón y choclo'},
+    { dia: 26, descripcion: 'Muslo de pollo, acompañada de salsa de teriyaki y papas al vapor'},
+    { dia: 27, descripcion: 'Tarta florentina'},
+    { dia: 28, descripcion: 'Carré de cerdo relleno salado con puré de papas'},
+    { dia: 29, descripcion: 'Ñoquis con tuco clásico'},
+    { dia: 30, descripcion: 'Milanesa de pollo con puré de papas y perejil'},
+    { dia: 31, descripcion: 'Pescado al horno con papas noisette'}
   ];
   }
 
@@ -113,7 +114,7 @@ getTodayPrincipalSweetOption(): OpcionSemana{
       { dia: 'X', descripcion: 'Tortilla de papas clásica con ensalada de tomates y cebolla' },
       { dia: 'J', descripcion: 'Ensalada de roast beef, mix de hojas verdes, papines y huevo' },
       { dia: 'V', descripcion: 'Spaguettis caprese' },
-      { dia: 'S', descripcion: 'Ensalada de papines, huevo duro, zanahorias, choclo, cherrys y pepinos' },
+      { dia: 'S', descripcion: 'Ensalada 100% vegetariana: hojas de lechuga crespa y mora, cous cous,pasas de arándanos rojos, maní tostado, rúcula, queso parmesano, vinagreta de rúcula, zanahoria y pepinos' },
       { dia: 'D', descripcion: 'Ensalada de lomito, choclo, lechuga, zanahorias y tomate. Aderezo salsa golf' },
     ];
     return opciones;
@@ -129,7 +130,7 @@ getTodayPrincipalSweetOption(): OpcionSemana{
       { dia: 'X', descripcion: 'Pechuga de pollo deshuesado sin piel con puré de zapallo' },
       { dia: 'J', descripcion: 'Pastel de zapallitos y carne, con graten de salsa blanca y queso magre sin sal' },
       { dia: 'V', descripcion: 'Churrasco desgrasado con ensalada de lechuga, tomate y zanahoria rallada' },
-      { dia: 'S', descripcion: 'Souffle de calabaza, zanahoria y brocolli' },
+      { dia: 'S', descripcion: 'Milhojas de verduras' },
       { dia: 'D', descripcion: 'Milanesas de calabaza y berenjen al horno acompañada de arroz integral' },
     ];
     return opciones;
@@ -139,8 +140,8 @@ getTodayPrincipalSweetOption(): OpcionSemana{
   getPostrePrincipal(){
 
     return [
-      { dia: 'L', descripcion: 'Fruta de estación'},
-      { dia: 'M', descripcion : 'Crema de caramelo'},
+      { dia: 'L', descripcion: 'Bizcochuelo de chocolate'},
+      { dia: 'M', descripcion : 'Crema de dulce de leche'},
       { dia: 'X', descripcion : 'Fruta de estación'},
       { dia: 'J', descripcion : 'Flan caramel'},
       { dia: 'V', descripcion : 'Fruta de estación'},
